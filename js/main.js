6dec45200072f6168273be4844d94d3fac40495d
ezreal <tornike.zhizhiashvili@geolab.edu.ge>
2018-09-16 21:02:54 +0400

    Add Homepage

---
+++ js/main.js
@@ -0,0 +1,42 @@
+
+
+
+$(window).ready(function(){
+
+$('.news_slide').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+$('.partners_slide').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:6
+        }
+    }
+})
+
+
+})
+
